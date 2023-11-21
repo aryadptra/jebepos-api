fe1fee46f957edc16d2326ebc803815e6ce741d3
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-11-21 18:49:15 +0700

    configuration authentication

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ResponseResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'success'   => false,
+                'message' => ['These credentials do not match our records.']
+            ], 404);
+        }
+
+        $token = $user->createToken('ApiToken')->plainTextToken;
+
+        $response = [
+            'success'   => true,
+            'user'      => $user,
+            'token'     => $token
+        ];
+
+        return new ResponseResource(true, 'Login successful', $response);
+    }
+}
