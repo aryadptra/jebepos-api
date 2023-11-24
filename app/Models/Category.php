d2eb276e501da798c05f7988065a43482d87933f
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-11-24 07:05:22 +0700

    update created at and updated at format

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,18 @@ class Category extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    // Definisikan mutator untuk atribut created_at
+    public function getCreatedAtAttribute($value)
+    {
+        // Ubah format atribut created_at menggunakan Carbon
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Definisikan mutator untuk atribut created_at
+    public function getUpdateddAtAttribute($value)
+    {
+        // Ubah format atribut created_at menggunakan Carbon
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
 }
