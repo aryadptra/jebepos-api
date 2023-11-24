4301c3f08ce792493053dfe7458042822bdd40f9
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-11-24 08:32:43 +0700

    update mutator timestamp

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,20 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    // Definisikan mutator untuk atribut created_at
+    public function getCreatedAtAttribute($value)
+    {
+        // Ubah format atribut created_at menggunakan Carbon
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    // Definisikan mutator untuk atribut created_at
+    public function getUpdateddAtAttribute($value)
+    {
+        // Ubah format atribut created_at menggunakan Carbon
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
 
     public function category()
     {
